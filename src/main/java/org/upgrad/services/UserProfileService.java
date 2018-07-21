b3bd4f7adc74c8c93fa01993be67cfc7a15fe18b
Akshar9015 <maulik.trivedi9015@gmail.com>
2018-07-21 18:24:43 +0530

    Update UserProfileService.java

--- src/main/java/org/upgrad/services/UserProfileService.java
+++ src/main/java/org/upgrad/services/UserProfileService.java
@@ -1,4 +1,11 @@
 package org.upgrad.services;
 
-public class UserProfileService {
+import org.springframework.stereotype.Service;
+import org.upgrad.models.User_Profile;
+
+@Service
+public interface UserProfileService {
+
+    public User_Profile retrieveUserProfile(int userId);
+
 }
