0a6728667a5c393881141d4f5b3de599a4b26004
Akshar9015 <maulik.trivedi9015@gmail.com>
2018-07-21 18:25:35 +0530

    Update UserService.java

--- src/main/java/org/upgrad/services/UserService.java
+++ src/main/java/org/upgrad/services/UserService.java
@@ -1,4 +1,29 @@
 package org.upgrad.services;
 
-public class UserService {
+import org.springframework.stereotype.Service;
+import org.upgrad.models.User;
+import org.upgrad.models.User_Profile;
+import java.util.List;
+
+@Service
+public interface UserService {
+
+    public String findUserByUsername(String userName);
+
+    public String findUserByEmail(String email);
+
+    public String findUserPassword(String userName);
+
+    public String findUserRole(String userName);
+
+    public int findUserId(String userName);
+
+    public Boolean registerUserDetails(User user, User_Profile uf);
+
+    public void deleteUserById(int id);
+
+    public List<User> getAllUsers();
+
+    public User getUserByUsername(String userName);
+
 }
