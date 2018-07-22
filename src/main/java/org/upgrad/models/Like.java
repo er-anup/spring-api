ae49e5fe85ab4f84b98cfeaea8e2222edd70bb59
Akshar9015 <maulik.trivedi9015@gmail.com>
2018-07-22 15:10:24 +0530

    Like class defined.

---
+++ src/main/java/org/upgrad/models/Like.java
@@ -0,0 +1,36 @@
+package org.upgrad.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Like {
+    @Id
+    private int id;
+    private int user_id;
+    private int answer_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getAnswer_id() {
+        return answer_id;
+    }
+
+    public void setAnswer_id(int answer_id) {
+        this.answer_id = answer_id;
+    }
+}
