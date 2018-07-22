717543bad17ddba717d419d003283263dd79a855
Akshar9015 <maulik.trivedi9015@gmail.com>
2018-07-22 15:06:50 +0530

    Follow class defined.

---
+++ src/main/java/org/upgrad/models/Follow.java
@@ -0,0 +1,36 @@
+package org.upgrad.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Follow {
+    @Id
+    private int id;
+    private int user_id;
+    private int category_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+}
